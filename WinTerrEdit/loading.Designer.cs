63c4d2c8ae10ef6975647c7795c07b368dee73e5
nymda <puffleboy101@hotmail.co.uk>
2020-06-29 22:41:54 +0100

    Made splash screen black. Not political, just looks cool.

--- WinTerrEdit/loading.Designer.cs
+++ WinTerrEdit/loading.Designer.cs
@@ -72,7 +72,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(393, 91);
+            this.ClientSize = new System.Drawing.Size(393, 92);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
