897bb66f1c7888887382ff8934e63d7e765e1702
jake hartley <puffleboy101@hotmail.co.uk>
2020-09-22 10:20:59 +0100

    1.10.1 release

--- WinTerrEdit/loading.Designer.cs
+++ WinTerrEdit/loading.Designer.cs
@@ -65,7 +65,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Loading | WTE 1.10.0";
+            this.label1.Text = "Loading | WTE 1.10.1";
             // 
             // loading
             // 
